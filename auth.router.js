f2ef4c5bb6a6d2f39a1fe4b13a13cef8b51e1507
Thanseer. T <tthanseer407@gmail.com>
2023-10-25 21:26:44 +0530

    Added middleware function for error handling

--- auth.router.js
+++ auth.router.js
@@ -1,9 +1,21 @@
 import express from "express";
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 const router = express.Router();
 
-router.post("/signup", function(req, res){  
-    console.log(req.body);
+router.post("/signup", async function(req, res, next){  
+    
+    const {username, email, password} = req.body;
+    const hashedpasssword =  bcryptjs.hashSync(password, 10);
+    const newUser = new User({username, email, password: hashedpasssword});
+
+    try {
+        await  newUser.save();
+        res.status(201).json("user created succesfully");
+    } catch (error) {
+        next(error);
+    }
 });
 
 
--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import authRouter from './routes/auth.router.js';
 
 const app = express();
 
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://127.0.0.1:27017/UserDB", {useNewUrlParser: true}).then(() => {
+mongoose.connect("mongodb://127.0.0.1:27017/EstateDB", {useNewUrlParser: true}).then(() => {
     console.log("success");
 }).catch((err) => {
     console.log(err);
